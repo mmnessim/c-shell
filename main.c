06d45ab8bb48f393c2bfd3514847e7cad2c5c35f
Mounir Nessim <mmnessim@gmail.com>
2025-05-03 20:23:46 -0400

    begin implementing repl()

--- main.c
+++ main.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
+#include <string.h>
 #include "utils.h"
 
-int main(int argc, char** argv) {
+void repl();
+
+int main() {
     //if (argc < 2) {
     //    printf("useage c-shell <filename>\n");
     //    return 0;
@@ -12,5 +15,20 @@ int main(int argc, char** argv) {
     //cat(filename);
 
     ls(".");
+    repl();
     return 0;
+}
+
+void repl() {
+    while (1) {
+        printf("$ ");
+        char input[256];
+
+        fgets(input, 256, stdin);
+        input[strcspn(input, "\n")] = 0;
+
+        if (strncmp("cat", input, strlen("cat")) == 0) {
+            cat("../main.c");
+        }
+    }
 }
